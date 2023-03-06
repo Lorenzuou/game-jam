c38c8d21e767a70e01012bc598ede1336e8dadd4
Lorenzo Scaramussa <lorenzoscaramussa@hotmail.com>
2023-03-06 15:20:01 +0000

    agora temos alguma coisa

--- start.js
+++ start.js
@@ -1,49 +1,74 @@
-import Cat from "./cat.js";
-// Initialize Phaser3
-const config = {
+// Initialize Phaser
+var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false
-    }
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 300 },
+          debug: false
+      }
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+      preload: preload,
+      create: create,
+      update: update
   }
 };
+var game = new Phaser.Game(config);
 
-const game = new Phaser.Game(config);
+// Declare global variables
+var player;
+var platforms;
+var lastJumpTime = 0;
+var jumpDelay = 500; // milliseconds
 
 
 function preload() {
-  this.load.spritesheet('cat', 'assets/dude.png', { frameWidth: 32, frameHeight: 32 });
+  // Load assets
+  this.load.image('sky', 'https://www.phaser.io/examples/v3/assets/skies/space3.png');
 }
 
 function create() {
-  // Create cat instance
-  const cat = new Cat(this, 100, 450);
+  // Add background
+  this.add.image(400, 300, 'sky');
 
-  // Add input handling code
-  const cursors = this.input.keyboard.createCursorKeys();
-  this.input.on('pointerdown', () => {
-    if (cat.body.onFloor()) {
-      cat.setVelocityY(-cat.jumpHeight);
-    }
-  });
+  // Add platforms
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+  platforms.create(800, 400, 'platform');
+  platforms.create(50, 250, 'platform');
+  platforms.create(750, 220, 'platform');
+
+  // Add player
+  player = this.physics.add.sprite(0, 0, 'player');
+  player.setBounce(0);
+  player.setCollideWorldBounds(true);
+  player.setGravityY(200);
+
+  // Add keyboard controls
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // Add collisions
+  this.physics.add.collider(player, platforms);
+  player.setCollideWorldBounds(true);
+
 }
 
 function update() {
-  // Call cat update method with cursors object
-  cat.update(cursors);
-  cat.draw();
-
-  //draw background
-  this.add.image(400, 300, 'sky');
+  // Add keyboard controls
+  if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+  } else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+  } else {
+      player.setVelocityX(0);
+  }
   
+
+  if (cursors.up.isDown && player.body.touching.down && (Date.now() - lastJumpTime > jumpDelay)) {
+    player.setVelocityY(-330);
+    lastJumpTime = Date.now();
+  }
 }
